5ef0e63e0e159904cd96f1983d89e8e9aa83cb20
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-05-27 23:46:19 +0200

    deploy nextjs project

---
+++ components/CallToActionButton/CallToActionButton.js
@@ -0,0 +1,19 @@
+import { ButtonLink } from "components/ButtonLink"
+
+export const CallToActionButton = ({  buttonLabel, destination, align = "left" }) => {
+  
+  const alignMap = {
+    left: "text-left",
+    center: "text-center",
+    right: "text-right"
+  }
+  
+  return (
+    <div className={alignMap[align]}>
+      <ButtonLink
+        destination={destination}
+        label={buttonLabel}
+      />
+    </div>
+  )
+}
